43a5e76c4b68ce3a9ea64b9dc8095ff7690cbd4d
Art Navsegda <artnavsegda@gmail.com>
2019-10-03 14:10:13 +0300

    file

--- src/file.c
+++ src/file.c
@@ -5,7 +5,7 @@ int main()
 {
   FILE *jsonfile = fopen("test.json", "r");
   char buffer[1000];
-  fread(buffer, sizeof(buffer), jsonfile);
+  fread(buffer, sizeof(buffer), 1, jsonfile);
 
 	return 0;
 }
