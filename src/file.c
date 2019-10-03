787effa6b371da88c4b0592fb53e90f51e3338e3
Art Navsegda <artnavsegda@gmail.com>
2019-10-03 14:44:54 +0300

    all

--- src/file.c
+++ src/file.c
@@ -7,7 +7,15 @@ int main()
   char buffer[1000];
   fread(buffer, sizeof(buffer), 1, jsonfile);
   json_object *myobj = json_tokener_parse(buffer);
-  puts(json_object_to_json_string(myobj));
+//  puts(json_object_to_json_string(myobj));
+
+  json_object_object_foreach(myobj, key, val)
+  {
+    puts(key);
+    puts(json_object_to_json_string(val));
+  }
+
+  puts(json_object_to_json_string(json_object_object_get(myobj,"name")));
 
 	return 0;
 }
