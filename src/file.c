8d86976a698260105b3a520afbc440c2e60a0000
Art Navsegda <artnavsegda@gmail.com>
2019-10-03 14:28:40 +0300

    file

--- src/file.c
+++ src/file.c
@@ -6,6 +6,8 @@ int main()
   FILE *jsonfile = fopen("test.json", "r");
   char buffer[1000];
   fread(buffer, sizeof(buffer), 1, jsonfile);
+  json_object *myobj = json_tokener_parse(buffer);
+  puts(json_object_to_json_string(myobj));
 
 	return 0;
 }
