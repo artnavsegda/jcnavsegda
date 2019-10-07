ec858dd27b6a4e474d2f6e2a2b1f49d973b4be23
Art Navsegda <artnavsegda@gmail.com>
2019-10-07 09:10:09 +0300

    all of this

--- src/file.c
+++ src/file.c
@@ -7,14 +7,8 @@
 
 int main()
 {
-  int jsonfile = open("test.json", O_RDONLY);
-  struct stat st;
-  fstat(jsonfile, &st);
-  char *buffer = malloc(st.st_size);
-  read(jsonfile, buffer, st.st_size);
-  json_object *myobj = json_tokener_parse(buffer);
-//  puts(json_object_to_json_string(myobj));
-
+  json_object *myobj = json_object_from_file("test.json");
+    
   json_object_object_foreach(myobj, key, val)
   {
     puts(key);
