bd9d541d9790381d25ce2200c5ce9eaaadcff201
Art Navsegda <artnavsegda@gmail.com>
2019-10-07 16:25:01 +0300

    file

--- src/file.c
+++ src/file.c
@@ -7,13 +7,19 @@
 
 int main()
 {
-  json_object *myobj = json_object_from_file("test.json");
+  json_object *myobj = json_object_from_file("test2.json");
+
+  puts(json_object_to_json_string(myobj));
+
+//  return 0;
     
   json_object_object_foreach(myobj, key, val)
   {
     puts(key);
     puts(json_object_to_json_string(val));
   }
+
+  return 0;
 
   json_object *name = NULL;
 
