972d4f1837a6f5a19c4438b49e671a20e8de0b0b
Art Navsegda <artnavsegda@gmail.com>
2020-01-08 09:53:00 +0300

    dup

--- src/test2.c
+++ src/test2.c
@@ -4,18 +4,26 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <json-c/json.h>
+#include <string.h>
 
 int main()
 {
-  json_object *myobj = json_object_from_file("test3.json");
+  json_object *obj = json_object_from_file("test3.json");
 
-  puts(json_object_to_json_string(myobj));
+  puts(json_object_to_json_string(obj));
 
-  json_object_object_foreach(myobj, key, val)
+  int i = 0;
+  char ** envp = NULL;
+  char tmpstr[1000];
+  json_object_object_foreach(obj, key, val)
   {
-    puts(key);
-    puts(json_object_to_json_string(val));
+    envp = realloc(&envp, sizeof(envp)*(i+1));
+    snprintf(tmpstr,1000,"__%s=%s",key,json_object_to_json_string(val));//check for leaks plz
+    envp[i]=strdup(tmpstr);
+    i++;
   }
+  envp = realloc(&envp, sizeof(envp)*(i+1));
+  envp[i]=NULL;
 
   return 0;
 }
